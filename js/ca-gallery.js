8c5aa899a8ce2e0ae942e49f2f1c09dbfd5e15e2
yaron ben yair <yaronby10@gmail.com>
2017-11-07 18:33:39 +0200

    try to puse

--- js/ca-gallery.js
+++ js/ca-gallery.js
@@ -5,7 +5,7 @@ var gProj = [{
   desc: "lorem ipsum lorem ipsum lorem ipsum",
   publishedAt: 1448693940000,
   labels: ["Matrixes", "keyboard events"],
-  link: 'games/sokoban'
+  link: 'games/sokoban/index.html'
 },
 {
   id: "mineSweeper",
@@ -24,7 +24,7 @@ var gProj = [{
   desc: "lorem ipsum lorem ipsum lorem ipsum",
   publishedAt: 1448693940000,
   labels: ["Matrixes", "keyboard events"],
-  link: 'games/touchNums'
+  link: 'games/touchNums/index.html'
   
 },
 {
@@ -34,7 +34,7 @@ title: "the calculator",
 desc: "lorem ipsum lorem ipsum lorem ipsum",
 publishedAt: 1448693940000,
 labels: ["Matrixes", "keyboard events"],
-link: 'games/calcu'
+link: 'games/calcu/index.html'
 
 },
 {
@@ -44,7 +44,7 @@ link: 'games/calcu'
   desc: "lorem ipsum lorem ipsum lorem ipsum",
   publishedAt: 1448693940000,
   labels: ["Matrixes", "keyboard events"],
-  link: 'games/guessNum'
+  link: 'games/guessNum/index.html'
   
   },
   {
@@ -54,7 +54,7 @@ link: 'games/calcu'
     desc: "lorem ipsum lorem ipsum lorem ipsum",
     publishedAt: 1448693940000,
     labels: ["Matrixes", "keyboard events"],
-    link: 'games/inPicture'
+    link: 'games/inPicture/index.html'
     
     }
 ]
